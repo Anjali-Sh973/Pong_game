24f68272115cfb6c62547d011a62386b236370c5
Anjali sharma <anjalisharma95573@gmail.com>
2024-03-29 09:02:29 +0530

    new modification

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
 
@@ -10,15 +12,19 @@ screen.tracer(0)
 
 r_paddle = Paddle((350,0))
 l_paddle = Paddle((-350,0))
+ball = Ball()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
 screen.onkey(r_paddle.go_down, "Down")
-screen.onkey(l_paddle.go_up, "W")
-screen.onkey(l_paddle.go_down, "S")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
 
 is_game_on = True
 while is_game_on:
     screen.update()
+    time.sleep(0.1)
+    ball.move()
+
 
 screen.exitonclick()
--- paddle.py
+++ paddle.py
@@ -2,23 +2,18 @@ from turtle import Turtle
 
 
 class Paddle(Turtle):
-    def __init__(self, paddle_position):
-        super().__init__(self)
-        self.paddle()
-        self.segment = Turtle()
-        self.p = paddle_position
-
-    def paddle(self):
-        self.segment.shape("square")
-        self.segment.color("white")
-        self.segment.turtlesize(stretch_wid=5, stretch_len=1)
-        self.segment.penup()
-        self.segment.goto(self.p)
+    def __init__(self, position):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.turtlesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+        self.goto(position)
 
     def go_up(self):
-        new_y = self.segment.ycor() + 20
-        self.segment.goto(self.segment.xcor(), new_y)
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
 
     def go_down(self):
-        new_y = self.segment.ycor() - 20
-        self.segment.goto(self.segment.xcor(), new_y)
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
